8f6665b4c7199ed95fcd9b7df4386274f643b15b
Allan Ferreira <ferrerallan@gmail.com>
2025-03-10 17:34:38 -0300

    first commit

--- nodes.py
+++ nodes.py
@@ -6,7 +6,7 @@ from state import AgentState
 
 load_dotenv()
 
-
+# Reasoning
 def run_agent_reasoning_engine(state: AgentState):
     agent_outcome = react_agent_runnable.invoke(state)
     return {"agent_outcome": agent_outcome}
@@ -14,8 +14,9 @@ def run_agent_reasoning_engine(state: AgentState):
 
 tool_executor = ToolExecutor(tools)
 
-
+# Act
 def execute_tools(state: AgentState):
     agent_action = state["agent_outcome"]
     output = tool_executor.invoke(agent_action)
     return {"intermediate_steps": [(agent_action, str(output))]}
+
--- state.py
+++ state.py
@@ -3,7 +3,6 @@ from typing import Annotated, TypedDict, Union
 
 from langchain_core.agents import AgentAction, AgentFinish
 
-
 class AgentState(TypedDict):
     input: str
     agent_outcome: Union[AgentAction, AgentFinish, None]
